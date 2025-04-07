dd01c53cbd43d3331b6f086b43069efcc0602126
Mayuushi <canenciawalter@gmail.com>
2025-04-07 23:08:28 +0800

    Added Controller Folder

---
+++ frontend-mobile/app/src/main/java/edu/cit/syncra/Controller/ApiService.kt
@@ -0,0 +1,15 @@
+package edu.cit.syncra.Controller
+
+import edu.cit.syncra.DataClass.User
+import retrofit2.Response
+import retrofit2.http.Body
+import retrofit2.http.POST
+
+interface ApiService {
+
+    @POST("/api/users")
+    suspend fun createUser(@Body user: User): Response<Map<String, Any>>
+
+    @POST("/api/login")  // Assuming your backend API has a login endpoint at "/api/login"
+    suspend fun loginUser(@Body credentials: Map<String, String>): Response<Map<String, Any>>
+}
