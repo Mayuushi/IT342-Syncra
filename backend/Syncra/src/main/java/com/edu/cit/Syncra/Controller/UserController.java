22d494b56bd36da8f3af01f8932056e6ccc1edb3
Mira032203 <johnmichaelpogoy22@gmail.com>
2025-04-09 15:56:27 +0800

    Modified to accept frontend

--- backend/Syncra/src/main/java/com/edu/cit/Syncra/Controller/UserController.java
+++ backend/Syncra/src/main/java/com/edu/cit/Syncra/Controller/UserController.java
@@ -1,15 +1,25 @@
 package com.edu.cit.Syncra.Controller;
 
-import com.edu.cit.Syncra.Entity.User;
-import com.edu.cit.Syncra.Service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.edu.cit.Syncra.Entity.User;
+import com.edu.cit.Syncra.Service.UserService;
+
+@CrossOrigin(origins = "http://localhost:5173")
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
