0db39400c728490ba6a974168921943f95c966b0
Mira032203 <johnmichaelpogoy22@gmail.com>
2025-04-09 15:58:18 +0800

    Modified newsfeedController to accept frontend connection

--- backend/Syncra/src/main/java/com/edu/cit/Syncra/Controller/NewsFeedController.java
+++ backend/Syncra/src/main/java/com/edu/cit/Syncra/Controller/NewsFeedController.java
@@ -1,13 +1,22 @@
 package com.edu.cit.Syncra.Controller;
 
-import com.edu.cit.Syncra.Entity.NewsFeed;
-import com.edu.cit.Syncra.Service.NewsFeedService;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.util.*;
+import com.edu.cit.Syncra.Entity.NewsFeed;
+import com.edu.cit.Syncra.Service.NewsFeedService;
 
+@CrossOrigin(origins = "http://localhost:5173")
 @RestController
 @RequestMapping("/api/newsfeed")
 public class NewsFeedController {
