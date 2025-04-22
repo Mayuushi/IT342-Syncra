5b23d4a158d4c640829a336d7a1dbb603c9602ba
Mira032203 <johnmichaelpogoy22@gmail.com>
2025-04-22 15:17:28 +0800

    updated backend to connect with frotnedn cors policy

--- backend/Syncra/src/main/java/com/edu/cit/Syncra/Controller/PortfolioController.java
+++ backend/Syncra/src/main/java/com/edu/cit/Syncra/Controller/PortfolioController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.*;
 
+@CrossOrigin(origins = "https://it342-syncra-web.onrender.com")
 @RestController
 @RequestMapping("/api/portfolio")
 public class PortfolioController {
