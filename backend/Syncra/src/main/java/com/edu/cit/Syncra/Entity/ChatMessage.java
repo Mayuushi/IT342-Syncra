766c3c1080c684a6f8cf7615f3a649171f9b4030
Mira032203 <johnmichaelpogoy22@gmail.com>
2025-04-23 19:13:27 +0800

    changes again

--- backend/Syncra/src/main/java/com/edu/cit/Syncra/Entity/ChatMessage.java
+++ backend/Syncra/src/main/java/com/edu/cit/Syncra/Entity/ChatMessage.java
@@ -1,12 +1,17 @@
 package com.edu.cit.Syncra.Entity;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.time.LocalDateTime;
 
+@Setter
+@Getter
 @Document(collection = "chat_messages") // MongoDB collection name
 public class ChatMessage {
+    // Getters and Setters
     @Id
     private String id; // Unique identifier for MongoDB
     private String sender;
@@ -14,44 +19,4 @@ public class ChatMessage {
     private String content;
     private LocalDateTime timestamp;
 
-    // Getters and Setters
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getSender() {
-        return sender;
-    }
-
-    public void setSender(String sender) {
-        this.sender = sender;
-    }
-
-    public String getRecipient() {
-        return recipient;
-    }
-
-    public void setRecipient(String recipient) {
-        this.recipient = recipient;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
-
-    public LocalDateTime getTimestamp() {
-        return timestamp;
-    }
-
-    public void setTimestamp(LocalDateTime timestamp) {
-        this.timestamp = timestamp;
-    }
 }
