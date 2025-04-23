e6d07fccc20ba91dec6e506dedd6191f5a6bec34
Mira032203 <johnmichaelpogoy22@gmail.com>
2025-04-23 19:42:57 +0800

    fixing it again

--- backend/Syncra/src/main/java/com/edu/cit/Syncra/Config/WebSocketConfig.java
+++ backend/Syncra/src/main/java/com/edu/cit/Syncra/Config/WebSocketConfig.java
@@ -17,6 +17,9 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws").setAllowedOrigins("*").withSockJS();
+        registry.addEndpoint("/ws")
+                .setAllowedOrigins("https://it342-syncra-web.onrender.com") // <-- frontend domain
+                .withSockJS();
     }
+
 }
