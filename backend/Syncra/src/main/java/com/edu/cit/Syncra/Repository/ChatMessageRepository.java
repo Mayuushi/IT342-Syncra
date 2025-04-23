2c3dd2a9e3bf9e75db940d3d3c8a7181e3886caf
Mira032203 <johnmichaelpogoy22@gmail.com>
2025-04-23 19:08:30 +0800

    1

--- backend/Syncra/src/main/java/com/edu/cit/Syncra/Repository/ChatMessageRepository.java
+++ backend/Syncra/src/main/java/com/edu/cit/Syncra/Repository/ChatMessageRepository.java
@@ -2,9 +2,13 @@ package com.edu.cit.Syncra.Repository;
 
 import com.edu.cit.Syncra.Entity.ChatMessage;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface ChatMessageRepository extends MongoRepository<ChatMessage, String> {
-    List<ChatMessage> findBySenderIdAndReceiverIdOrReceiverIdAndSenderId(String sender1, String receiver1, String sender2, String receiver2);
+// Correct method names
+List<ChatMessage> findBySenderAndRecipient(String sender, String recipient);
+List<ChatMessage> findByRecipient(String recipient);
 }
