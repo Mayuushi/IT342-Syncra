b3bfeceaadffb3bdb7ba68c54562e9ae9823e0a4
LAB23_USER ON WS31 <L23X14W31@citulab.net>
2025-04-25 12:49:15 +0800

    change margin

--- frontend-web/My-Syncra/src/Components/Sections/Login.jsx
+++ frontend-web/My-Syncra/src/Components/Sections/Login.jsx
@@ -11,13 +11,13 @@ const Login = () => {
         <span className="ml-2 text-3xl font-bold text-blue-600">Syncra.</span>
       </div>
 
-      <div className="sm:mx-auto sm:w-full sm:max-w-md mb-20 mr-30">
+      <div className="sm:mx-auto sm:w-full sm:max-w-md mb-20">
         <h2 className="mt-0 text-center text-3xl font-extrabold text-gray-900">
           Sign in to your account
         </h2>
       </div>
 
-      <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
+      <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md mb-28">
         <div className="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
           <LoginForm />
         </div>
