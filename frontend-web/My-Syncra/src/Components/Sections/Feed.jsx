d531a4328e7dfbdfcc93d1689065447356ad3294
Mira032203 <johnmichaelpogoy22@gmail.com>
2025-04-25 21:22:33 +0800

    Fixed imports

--- frontend-web/My-Syncra/src/Components/Sections/Feed.jsx
+++ frontend-web/My-Syncra/src/Components/Sections/Feed.jsx
@@ -8,8 +8,8 @@ import {
   FiSend,
   FiMoreHorizontal,
 } from 'react-icons/fi';
-import authService from '../services/authService';
-import newsFeedService from '../services/newsFeedService';
+import authService from '../../services/authService';
+import newsFeedService from '../../services/newsFeedService';
 
 function Feed() {
   const navigate = useNavigate();
