47624c184e7d0f8a62ab51f7653405104959a00e
ShaneAdrianOpinion <shaneopinion95@gmail.com>
2025-05-02 11:13:33 +0800

    portfolio modified

--- frontend-web/My-Syncra/src/Components/Sections/Portfolio.jsx
+++ frontend-web/My-Syncra/src/Components/Sections/Portfolio.jsx
@@ -52,7 +52,7 @@ function Portfolio() {
           <div className="portfolio-profile-info">
             <div className="portfolio-profile-avatar">
               <img
-                src={spacexImg}
+                src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
                 alt="Profile"
                 className="portfolio-profile-avatar-img"
               />
