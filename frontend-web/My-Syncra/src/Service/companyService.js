c09ad6d2e71fb06c78817872cd20f2c1d5133f9d
Mira032203 <johnmichaelpogoy22@gmail.com>
2025-05-06 19:09:57 +0800

    Removed authHeader and revised companyService

--- frontend-web/My-Syncra/src/Service/companyService.js
+++ frontend-web/My-Syncra/src/Service/companyService.js
@@ -1,55 +1,40 @@
 import axios from "axios";
-import authHeader from "./authHeader";
 const API_URL = "/api/companies";
 
 class CompanyService {
   // Create a new company
   createCompany(companyData, userId) {
-    return axios.post(`${API_URL}?userId=${userId}`, companyData, {
-      headers: authHeader(),
-    });
+    return axios.post(`${API_URL}?userId=${userId}`, companyData);
   }
 
   // Get company by ID
   getCompanyById(companyId) {
-    return axios.get(`${API_URL}/${companyId}`, {
-      headers: authHeader(),
-    });
+    return axios.get(`${API_URL}/${companyId}`);
   }
 
   // Get companies owned by user
   getUserCompanies(userId) {
-    return axios.get(`${API_URL}/user/${userId}`, {
-      headers: authHeader(),
-    });
+    return axios.get(`${API_URL}/user/${userId}`);
   }
 
   // Update company details
   updateCompany(companyId, companyData, userId) {
-    return axios.put(`${API_URL}/${companyId}?userId=${userId}`, companyData, {
-      headers: authHeader(),
-    });
+    return axios.put(`${API_URL}/${companyId}?userId=${userId}`, companyData);
   }
 
   // Delete a company
   deleteCompany(companyId, userId) {
-    return axios.delete(`${API_URL}/${companyId}?userId=${userId}`, {
-      headers: authHeader(),
-    });
+    return axios.delete(`${API_URL}/${companyId}?userId=${userId}`);
   }
 
   // Post a job as a company
   postJob(companyId, jobData, userId) {
-    return axios.post(`${API_URL}/${companyId}/jobs?userId=${userId}`, jobData, {
-      headers: authHeader(),
-    });
+    return axios.post(`${API_URL}/${companyId}/jobs?userId=${userId}`, jobData);
   }
 
   // Get all jobs posted by a company
   getCompanyJobs(companyId) {
-    return axios.get(`${API_URL}/${companyId}/jobs`, {
-      headers: authHeader(),
-    });
+    return axios.get(`${API_URL}/${companyId}/jobs`);
   }
 
   // Get all companies (for browsing)
