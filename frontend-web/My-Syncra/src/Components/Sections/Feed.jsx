7f624afdf7650251e6ff1c5ed3eb48749a1e32ab
ShaneAdrianOpinion <shaneopinion95@gmail.com>
2025-05-08 16:40:03 +0800

    Changes a lot of shinanigans'

--- frontend-web/My-Syncra/src/Components/Sections/Feed.jsx
+++ frontend-web/My-Syncra/src/Components/Sections/Feed.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-import NavBar from '../NavBar';
+import { NavBar } from '../NavBar';
 import {
   FiHeart,
   FiMessageSquare,
